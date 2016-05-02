776062bbdcb07c9d8f63b4b9b49aa7f7897d5113
mbenlioglu <mbenlioglu@sabanciuniv.edu>
2016-05-02 14:16:39 +0300

    small changes

--- app/src/main/java/edu/sabanciuniv/cs310/pokemonworld/PokedexActivity.java
+++ app/src/main/java/edu/sabanciuniv/cs310/pokemonworld/PokedexActivity.java
@@ -18,7 +18,7 @@ public class PokedexActivity extends AppCompatActivity implements AsyncResponse 
     private String [] pokedex = {"Bulbasaur","Ivysaur","Venusaur","Charmander","Charmeleon","Charizard","Squirtle",
             "Wartortle","Blastoise","Caterpie","Metapod","Butterfree","Weedle","Kakuna","Beedrill","Pidgey",
             "Pidgeotto","Pidgeot","Rattata","Raticate","Spearow","Fearow","Ekans","Arbok","Pikachu","Raichu",
-            "Sandslash","Nidoran-m","Nidorina","Nidoqueen","Nidoran-f","Nidorino","Nidoking","Clefairy","Clefable",
+            "Sandslash","Nidoran_m","Nidorina","Nidoqueen","Nidoran_f","Nidorino","Nidoking","Clefairy","Clefable",
             "Vulpix","Ninetales","Jigglypuff","Wigglytuff","Zubat","Golbat","Oddish","Gloom","Vileplume",
             "Paras","Parasect","Venonat","Venomoth","Diglett","Dugtrio","Meowth","Persian","Psyduck","Golduck",
             "Mankey","Primeape","Growlithe","Arcanine","Poliwag","Poliwhirl","Poliwrath","Abra","Kadabra",
@@ -27,7 +27,7 @@ public class PokedexActivity extends AppCompatActivity implements AsyncResponse 
             "Doduo","Dodrio","Seel","Dewgong","Grimer","Muk","Shellder","Cloyster","Gastly","Haunter","Gengar","Onix",
             "Drowzee","Hypno","Krabby","Kingler","Voltorb","Electrode","Exeggcute","Exeggutor","Cubone","Marowak","Hitmonlee",
             "Hitmonchan","Lickitung","Koffing","Weezing","Rhyhorn","Rhydon","Chansey","Tangela","Kangaskhan","Horsea","Seadra",
-            "Goldeen","Seaking","Staryu","Starmie","Mr. Mime","Scyther","Jynx","Electabuzz","Magmar","Pinsir","Tauros","Magikarp",
+            "Goldeen","Seaking","Staryu","Starmie","Mr_Mime","Scyther","Jynx","Electabuzz","Magmar","Pinsir","Tauros","Magikarp",
             "Gyarados","Lapras","Ditto","Eevee","Vaporeon","Jolteon","Flareon","Porygon","Omanyte","Omastar","Kabuto","Kabutops",
             "Aerodactyl","Snorlax","Articuno","Zapdos","Moltres","Dratini","Dragonair","Dragonite","Mewtwo","Mew"};
 
